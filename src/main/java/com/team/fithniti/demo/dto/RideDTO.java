be2aa5ed77ae08590519559926b6e5927e57b6f8
hamdi-hs <hamdi.hassen@eniso.u-sousse.tn>
2021-10-30 11:55:21 +0100

    add method for mapping Ride Entity into DTO

--- src/main/java/com/team/fithniti/demo/dto/RideDTO.java
+++ src/main/java/com/team/fithniti/demo/dto/RideDTO.java
@@ -1,6 +1,8 @@
 package com.team.fithniti.demo.dto;
 
+import com.team.fithniti.demo.exception.InvalidResource;
 import com.team.fithniti.demo.model.Driver;
+import com.team.fithniti.demo.model.Ride;
 import com.team.fithniti.demo.model.RidePathway;
 import com.team.fithniti.demo.model.Tag;
 import com.team.fithniti.demo.util.RideState;
@@ -33,4 +35,26 @@ public class RideDTO {
     private LocalTime arrivalTime;
     private Driver driver; // TODO: 10/30/21 replace Driver with DriverDTO
     List<Tag> tags;// TODO: 10/30/21 same thing, replace with DTO + check if tag is not null before mapping
+
+    // this method allows to map a Ride Entity into RideDTO
+    public static RideDTO fromEntity(Ride ride){
+        if ( ride != null ) {
+            return RideDTO.builder()
+                    .id(ride.getId())
+                    .description(ride.getDescription())
+                    .pathway(ride.getPathway())
+                    .rideType(ride.getRideType())
+                    .rideState(ride.getRideState())
+                    .availablePlaces(ride.getAvailablePlaces())
+                    .maxPlaces(ride.getMaxPlaces())
+                    .rating(ride.getRating())
+                    .ratersNumber(ride.getRatersNumber())
+                    .price(ride.getPrice())
+                    .startDate(ride.getCreatedDate())
+                    .startTime(ride.getStartTime())
+                    .arrivalTime(ride.getArrivalTime())
+                    .build();
+        }
+        throw new InvalidResource(null,"INVALID_ENTITY","Can't map null entity");
+    }
 }
