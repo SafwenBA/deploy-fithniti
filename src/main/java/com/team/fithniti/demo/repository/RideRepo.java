64878b12da7b0cb41bd6467593529570a0885406
Safwen Ben Abdessalem <safwenba1@gmail.com>
2021-10-31 01:32:00 +0100

    added Passenger service, Driver service, Image service missing impl

--- src/main/java/com/team/fithniti/demo/repository/RideRepo.java
+++ src/main/java/com/team/fithniti/demo/repository/RideRepo.java
@@ -1,8 +1,12 @@
 package com.team.fithniti.demo.repository;
 
 import com.team.fithniti.demo.model.Ride;
+import com.team.fithniti.demo.model.RideRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RideRepo extends JpaRepository<Ride,Long> {
+import java.util.UUID;
 
+public interface RideRepo extends JpaRepository<Ride,Long> {
 }
