b1bd1f8b019c46751da445dd0590dcbd06b04c1f
hamdi-hs <hamdi.hassen@eniso.u-sousse.tn>
2021-11-04 16:21:28 +0100

    add implementation for ResourceNotFound exception

--- src/main/java/com/team/fithniti/demo/exception/ResourceNotFound.java
+++ src/main/java/com/team/fithniti/demo/exception/ResourceNotFound.java
@@ -1,4 +1,19 @@
 package com.team.fithniti.demo.exception;
 
-public class ResourceNotFound {
+import java.util.List;
+
+public class ResourceNotFound extends RuntimeException{
+    private List<String> errors;
+    private String errorCode;
+    private String msg;
+
+    public ResourceNotFound(List<String> errors, String errorCode, String msg) {
+        super(msg);
+        this.errors = errors;
+        this.errorCode = errorCode;
+    }
+    public ResourceNotFound(String errorCode, String msg) {
+        super(msg);
+        this.errorCode = errorCode;
+    }
 }
