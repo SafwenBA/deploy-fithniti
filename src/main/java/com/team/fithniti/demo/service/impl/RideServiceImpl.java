d963d0422c94364a148713e7649aa571f65505f8
hamdi-hs <hamdi.hassen@eniso.u-sousse.tn>
2021-11-04 16:22:26 +0100

    add some impls for RideService

--- src/main/java/com/team/fithniti/demo/service/impl/RideServiceImpl.java
+++ src/main/java/com/team/fithniti/demo/service/impl/RideServiceImpl.java
@@ -1,62 +1,98 @@
 package com.team.fithniti.demo.service.impl;
 
+import com.team.fithniti.demo.dto.RideFilterOption;
+import com.team.fithniti.demo.dto.request.NewRide;
+import com.team.fithniti.demo.dto.response.RideDTO;
+import com.team.fithniti.demo.dto.response.TagDto;
+import com.team.fithniti.demo.exception.InvalidResource;
+import com.team.fithniti.demo.exception.ResourceNotFound;
+import com.team.fithniti.demo.model.AppUser;
+import com.team.fithniti.demo.model.Driver;
 import com.team.fithniti.demo.model.Ride;
 import com.team.fithniti.demo.repository.RideRepo;
 import com.team.fithniti.demo.service.RideService;
+import com.team.fithniti.demo.util.Order;
+import com.team.fithniti.demo.validator.RideValidation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Map;
+
 
 @Service
-public class RideServiceImpl implements RideService {
+public class RideServiceImpl implements RideService{
     @Autowired
     private RideRepo rideRepo;
 
+
     @Override
-    public Ride create(Ride ride) {
-        // TODO: 10/29/21 check before if not exists ....
-        return rideRepo.save(ride);
+    public RideDTO create(NewRide ride) {
+        if ( ride == null )
+            throw new InvalidResource(null,"INVALID_RIDE","Can't persist null entity");
+        // driverService.findById(ride.GetIdDriver)
+
+        Driver driver = new Driver(); // == driverService.findById(ride.getDriverId)
+        AppUser user = driver.getUser();
+        // checkUserAccess(user) ; //
+//        Example
+//        private void checkUserAccess(AppUser user){
+//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//            AppUser authenticatedUser = userDao.findByLogin(authentication.getName()).orElseThrow(() -> new ResourceNotFoundException("Oops!, Something went wrong"));
+//            if ( ! user.equals(authenticatedUser) && !authenticatedUser.isAdmin() ) {
+//                throw new AccessDeniedException("Oops! You don't have permissions!");
+//            }
+//        }
+
+        // end checkUser
+
+//        CarService.findByBrand(ride.getCar().getBrand(),ride.getCar().getCarModel());
+
+        return RideDTO.fromEntity(
+                rideRepo.save( Ride.builder()
+                        .description(ride.getDescription())
+                        .startTime(ride.getStartTime())
+                        .maxPlaces(ride.getMaxPlaces())
+                        .pathway(ride.getPathway())
+                        .price(ride.getPrice())
+                        .rideType(ride.getRideType())
+                        .tags(ride.getTags())
+                        .driver(driver)
+                        .build()
+                )
+        );
     }
 
     @Override
-    public List<Ride> getAll() {
-        return rideRepo.findAll();
+    public RideDTO findById(Long id) {
+        return RideDTO.fromEntity(rideRepo.findById(id).orElseThrow(
+                () -> new ResourceNotFound("INVALID_RIDE","No Ride found")));
     }
 
     @Override
-    public List<Ride> getDriverRides(Long driverId) {
+    public Page<RideDTO> findAll(RideFilterOption options) {
+        // TODO: 11/4/21 impl the rest
+        Sort.Direction sort = ( options.getOrder().equals(Order.ASC)) ? Sort.Direction.ASC : Sort.Direction.DESC ;
+        PageRequest pageRequest = PageRequest.of(options.getPage(), options.getLimit(), sort,options.getProperties());
+        return  rideRepo.findAll(pageRequest).map(RideDTO::fromEntity);
+    }
+
+    @Override
+    public Page<RideDTO> findDriverRides(Long driverId, RideFilterOption options) {
+        // TODO: 11/4/21 impl the rest + Q_DSL
         return null;
     }
 
     @Override
-    public void deleteById(Long id) {
-        // TODO: 10/29/21 check before if not exists ....
-        rideRepo.deleteById(id);
+    public void deleteById(Long rideId) {
+        rideRepo.deleteById(findById(rideId).getId());
     }
 
     @Override
-    public void update(Long rideId) {
-
-    }
-
-    @Override
-    public void joinRide(Long rideID, Long passengerID) {
-
-    }
-
-    @Override
-    public void acceptRequest(Long rideID) {
-
-    }
-
-    @Override
-    public void rejectRequest(Long rideID) {
-
-    }
-
-    @Override
-    public void cancelRequest(Long rideID, Long passengerID) {
-
+    public void update(Long rideId, Long driverId, Map<String, Object> changes) {
+        // TODO: 11/4/21 impl
     }
 }
