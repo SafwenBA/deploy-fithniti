6b13a4c5bcf45a6590e2c775908cd768096eca76
hamdi-hs <hamdi.hassen@eniso.u-sousse.tn>
2021-11-04 15:21:27 +0100

    create repository for Ride Entity

--- src/main/java/com/team/fithniti/demo/repository/RideRepo.java
+++ src/main/java/com/team/fithniti/demo/repository/RideRepo.java
@@ -1,8 +1,14 @@
 package com.team.fithniti.demo.repository;
 
+import com.team.fithniti.demo.model.Driver;
 import com.team.fithniti.demo.model.Ride;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RideRepo extends JpaRepository<Ride,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface RideRepo extends JpaRepository<Ride,Long> {
+    @Override
+    void delete(Ride entity);
 }
