c5eafeefff2b68af42f698bc8c0f7702186956ff
Safwen Ben Abdessalem <safwenba1@gmail.com>
2021-11-07 22:10:42 +0100

    latest ImageService + endpoints with Base64 string/ binary format

--- src/main/java/com/team/fithniti/demo/service/impl/ImageServiceImpl.java
+++ src/main/java/com/team/fithniti/demo/service/impl/ImageServiceImpl.java
@@ -1,16 +1,19 @@
 package com.team.fithniti.demo.service.impl;
 
+import com.team.fithniti.demo.dto.request.NewImage;
 import com.team.fithniti.demo.dto.request.NewLogo;
+import com.team.fithniti.demo.model.Image;
 import com.team.fithniti.demo.model.Logo;
 import com.team.fithniti.demo.repository.ImageRepo;
+import com.team.fithniti.demo.repository.LogoRepo;
 import com.team.fithniti.demo.service.ImageService;
-import lombok.extern.java.Log;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,6 +22,9 @@ import java.util.UUID;
 @Service
 public class ImageServiceImpl implements ImageService {
     @Autowired
+    private LogoRepo logoRepo;
+
+    @Autowired
     private ImageRepo imageRepo;
 
     private static final String DEFAULT_ID = "DEFAULT_LOGO_ID_STRING";
@@ -26,7 +32,7 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public Logo loadImage(UUID uuid) {
-        Optional<Logo> logo = this.imageRepo.findByUserId(uuid);
+        Optional<Logo> logo = this.logoRepo.findByUserId(uuid);
         if(logo.isPresent()){
             return logo.get();
         }else{
@@ -37,7 +43,7 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public Logo loadDefault() {
-        Optional<Logo> defaultLogo = this.imageRepo.findById(DEFAULT_ID);
+        Optional<Logo> defaultLogo = this.logoRepo.findById(DEFAULT_ID);
         if(defaultLogo.isPresent()){
             return defaultLogo.get();
         }else{
@@ -49,7 +55,7 @@ public class ImageServiceImpl implements ImageService {
     public void storeImage(UUID userId, MultipartFile file) {
         try{
             NewLogo newLogo = new NewLogo(userId, new Binary(BsonBinarySubType.BINARY, file.getBytes()));
-            this.imageRepo.save(Logo.builder()
+            this.logoRepo.save(Logo.builder()
                     .userId(newLogo.getUserId())
                     .image(newLogo.getImage())
                     .build());
@@ -61,14 +67,14 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public void deleteImage(UUID uuid) {
         Logo logo = loadImage(uuid);
-        this.imageRepo.delete(logo);
+        this.logoRepo.delete(logo);
     }
 
     @Override
     public void storeDefault(MultipartFile file) {
         try{
             NewLogo newLogo = new NewLogo(DEFAULT_UUID, new Binary(BsonBinarySubType.BINARY, file.getBytes()));
-            this.imageRepo.save(Logo.builder()
+            this.logoRepo.save(Logo.builder()
                     .id(DEFAULT_ID)
                     .userId(newLogo.getUserId())
                     .image(newLogo.getImage())
@@ -77,4 +83,89 @@ public class ImageServiceImpl implements ImageService {
             return;
         }
     }
+
+    //Working with base64 String
+    @Override
+    public Image addImage(UUID userId, MultipartFile file) {
+        try{
+            NewImage newImage = new NewImage(userId, toBase64(convertMultiPartToFile(file)));
+            Image image = Image.builder()
+                    .userId(userId)
+                    .image(newImage.getImage())
+                    .build();
+            this.imageRepo.save(image);
+            return image;
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    @Override
+    public Image getDefault() {
+        Optional<Image> image = this.imageRepo.findByUserId(DEFAULT_UUID);
+        if(image.isPresent()){
+            return image.get();
+        }else{
+            //TODO : Update Exceptions
+            throw new IllegalStateException("Logo not found");
+        }
+    }
+
+    @Override
+    public String getImageBase64(UUID uuid) {
+        Optional<Image> image = this.imageRepo.findByUserId(uuid);
+        if(image.isPresent()){
+            return image.get().getImage();
+        }else{
+            //TODO : Update Exceptions
+            throw new IllegalStateException("Logo not found");
+        }
+    }
+
+
+
+    @Override
+    public String getDefaultBase64() {
+        Optional<Image> defaultLogo = this.imageRepo.findById(DEFAULT_ID);
+        if(defaultLogo.isPresent()){
+            return defaultLogo.get().getImage();
+        }else{
+            throw new IllegalStateException("Logo not found");
+        }
+    }
+
+    @Override
+    public void addDefault(MultipartFile file) {
+        try{
+            NewImage newImage = new NewImage(DEFAULT_UUID, toBase64(convertMultiPartToFile(file)));
+            this.imageRepo.save(Image.builder()
+                    .id(DEFAULT_ID)
+                    .userId(newImage.getUserId())
+                    .image(newImage.getImage())
+                    .build());
+        }catch (IOException e){
+            return;
+        }
+    }
+
+    @Override
+    public Image getImage(UUID uuid) {
+        Optional<Image> image = this.imageRepo.findByUserId(uuid);
+        if(image.isPresent()){
+            return image.get();
+        }else{
+            //TODO : Update Exceptions
+            throw new IllegalStateException("Logo not found");
+        }
+    }
+
+
+
+    @Override
+    public Image delete(UUID uuid) {
+        Image image = getImage(uuid);
+        this.imageRepo.delete(image);
+        return image;
+    }
+
 }
