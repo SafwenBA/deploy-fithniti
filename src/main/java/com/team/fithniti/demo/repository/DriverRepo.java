6f5424897a9e9383c676f3959719da4b2dc79747
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-11-13 03:02:01 +0100

    repos updates

--- src/main/java/com/team/fithniti/demo/repository/DriverRepo.java
+++ src/main/java/com/team/fithniti/demo/repository/DriverRepo.java
@@ -1,9 +1,12 @@
 package com.team.fithniti.demo.repository;
 
+import com.team.fithniti.demo.model.AppUser;
 import com.team.fithniti.demo.model.Driver;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+import java.util.Optional;
 
 public interface DriverRepo extends JpaRepository<Driver, Long> {
+    Optional<Driver> findByUser(AppUser user);
 }
---
+++ src/main/java/com/team/fithniti/demo/repository/ReportTypeRepo.java
@@ -0,0 +1,7 @@
+package com.team.fithniti.demo.repository;
+
+import com.team.fithniti.demo.model.ReportType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportTypeRepo extends JpaRepository<ReportType, Long> {
+}
