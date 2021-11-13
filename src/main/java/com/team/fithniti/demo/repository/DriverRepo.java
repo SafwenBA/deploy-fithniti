685dd752a7e7248ddbc13cb03041ff379fec6968
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-11-13 04:56:47 +0100

    add methods to get passenger or driver with reportCounter greater or equal to x

--- src/main/java/com/team/fithniti/demo/repository/DriverRepo.java
+++ src/main/java/com/team/fithniti/demo/repository/DriverRepo.java
@@ -3,10 +3,14 @@ package com.team.fithniti.demo.repository;
 import com.team.fithniti.demo.model.AppUser;
 import com.team.fithniti.demo.model.Driver;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface DriverRepo extends JpaRepository<Driver, Long> {
     Optional<Driver> findByUser(AppUser user);
+
+    Page<Driver> getDriversByReportsCountGreaterThan(int reportCounter, Pageable pageable);
 }
