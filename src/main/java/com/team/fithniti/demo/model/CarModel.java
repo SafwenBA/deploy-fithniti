9f9c3f63d70aaa676d1661c827a29f710961c7e1
hamdi-hs <hamdi.hassen@eniso.u-sousse.tn>
2021-11-21 13:13:59 +0100

    bug fix

--- src/main/java/com/team/fithniti/demo/model/CarModel.java
+++ src/main/java/com/team/fithniti/demo/model/CarModel.java
@@ -1,5 +1,6 @@
 package com.team.fithniti.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,6 +26,7 @@ public class CarModel {
 
     @ManyToOne
     @JoinColumn(name="car_id", referencedColumnName = "id")
+    @JsonIgnore
     private Car car;
 
     public CarModel(String model, String imageURL, Car car) {
